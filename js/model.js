2489068f0d89e9e1fa49bb42007372302f92c9c7
Rajkumar <rajkumarsr1903@gmail.com>
2021-11-29 23:13:01 +0530

    etadpu

---
+++ js/model.js
@@ -0,0 +1,42 @@
+const EnggTextModel = {
+    color: 0x000000,
+    transparent: true,
+    message: "Create",
+    opacity: 0.8,
+    position_z: - window.innerWidth,
+    position_x: 0,
+    position_y: 0,
+    curveSegments: window.innerWidth * 0.18,
+    xMid: - 0.5,
+    translate_x: window.innerHeight,
+    translate_y: 0,
+}
+
+const ScientistTextModel = {
+    color: 0x000000,
+    transparent: true,
+    message: "Discover",
+    opacity: 0.8,
+    position_z: - window.innerWidth,
+    position_x: 0,
+    position_y: 0,
+    curveSegments: window.innerWidth * 0.18,
+    xMid: - 0.5,
+    translate_x: - window.innerHeight,
+    translate_y: 0,
+}
+
+const EducationTextModel = {
+    color: 0x000000,
+    transparent: true,
+    message: "Education",
+    opacity: 0.5,
+    position_z: - 300,
+    position_x: 0,
+    position_y: 0,
+    curveSegments: window.innerWidth * 0.18,
+    xMid: - 0.5,
+    translate_x: 300,
+    translate_y: 0,
+}
+
